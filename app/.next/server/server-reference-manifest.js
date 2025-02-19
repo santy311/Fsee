ecbf7827d61692a2802ce089a3876095cc08acb7
Santhosh <sansii1997@gmail.com>
2025-02-19 14:53:49 +0100

    removed .next folders

--- app/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"N3JsTRmGoNoRTIsMExEBwyrPR+sTqa0fG9nOAPO7aKU=\"\n}"
--- blinks/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"9c7C2GDkHSgKE4dF18fVKXHPnHcFYTiSjqVORCMlRXk=\"\n}"
